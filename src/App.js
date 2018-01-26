5d593d1860d0e27944545f6eef4c9cbf10125b48
Mark Chambers <me@marksy.com>
2018-01-26 10:04:15 +0000

    added textField component and minor reformatting of tag

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Form, Field } from 'react-final-form';
-import { Button, DateInput, Layout, GridRow, GridCol } from 'govuk-react';
+import { Button, InputField, DateInput, Layout, GridRow, GridCol } from 'govuk-react';
 
 // based on https://codesandbox.io/s/40mr0v2r87
 const DateInputAdapter = ({ input, meta, ...rest }) => (
@@ -11,13 +11,28 @@ const DateInputAdapter = ({ input, meta, ...rest }) => (
     errorText={meta.touched ? meta.error : ''}
   />
 );
+
+const InputFieldAdapter = ({ input, meta, ...rest }) => (
+  <InputField
+    {...input}
+    {...rest}
+    onChange={(event, value) => input.onChange(value)}
+    errorText={meta.touched ? meta.error : ''}
+  />
+)
+
+
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const onSubmit = async (values) => {
   await sleep(300);
   window.alert(JSON.stringify(values, 0, 2));
 };
+
 const required = value => (value ? undefined : 'Required');
+
+
+
 
 const App = () => (
   <div>
@@ -31,21 +46,35 @@ const App = () => (
       <GridRow>
         <GridCol>
           <Field
-            name="dob"
-            component={DateInputAdapter}
+            name="firstName"
+            component={InputFieldAdapter}
             validate={required}
-            hintText="Date of birth"
-            labelPosition="right"
-          />
+            hintText="e.g. John Smith"
+          >
+            Name
+          </Field>
         </GridCol>
       </GridRow>
       <GridRow>
         <GridCol>
+          <Field
+            name="dob"
+            component={DateInputAdapter}
+            validate={required}
+            hintText="31 3 1980"
+            labelPosition="right"
+          >
+          Date of birth
+          </Field>
+        </GridCol>
+      </GridRow>
+      <GridRow>
+        <GridCol columnOneQuarter>
           <Button type="submit" disabled={submitting}>
             Submit
           </Button>
         </GridCol>
-        <GridCol>
+        <GridCol columnOneQuarter>
           <Button
             type="button"
             onClick={reset}
@@ -54,6 +83,8 @@ const App = () => (
             Reset
           </Button>
         </GridCol>
+        <GridCol columnOneQuarter />
+        <GridCol columnOneQuarter />
       </GridRow>
       <GridRow>
         <GridCol>
